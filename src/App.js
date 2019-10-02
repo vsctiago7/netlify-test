ef40f5067f3363d1d7a6284a8b328a21c6b09ea5
Tiago Fernandes <vsctiago7@gmail.com>
2019-10-02 21:58:44 +0100

    Trying to get message from heroku server.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const msg = axios
+    .get("https://fercodes-herokutest.herokuapp.com/test")
+    .then(res => res.data)
+    .catch(err => {
+      console.log(err);
+      return "Error: Couldn't get message from server.";
+    });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a netlify test website.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is a netlify test website.</p>
+        <p>Message from server:</p>
+        <p>{msg}</p>
       </header>
     </div>
   );
