7355e6ee79b1ec3f27a89994eace4e6773e5d442
Tiago Fernandes <vsctiago7@gmail.com>
2019-10-03 02:38:18 +0100

    Trying to get serverless msg.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [msg, setMsg] = useState("Loading...");
+  const [smsg, setSMsg] = useState("Loading serverless message...");
 
   useEffect(() => {
     getServerMsg();
+    getServerlessMsg();
   });
 
-  const getServerMsg = () => axios
-    .get("https://fercodes-herokutest.herokuapp.com/test")
-    .then(res => setMsg(res.data))
-    .catch(err => {
-      console.log(err);
-      return "Error: Couldn't get message from server.";
-    });
+  const getServerMsg = () =>
+    axios
+      .get("https://fercodes-herokutest.herokuapp.com/test")
+      .then(res => setMsg(res.data))
+      .catch(err => {
+        console.log(err);
+        return "Error: Couldn't get message from server.";
+      });
+
+  const getServerlessMsg = () =>
+    axios
+      .get("https://fercodes.netlify.com/.netlify/functions/serverless/hello")
+      .then(res => setSMsg(res.data))
+      .catch(err => {
+        console.log(err);
+        return "Error: Couldn't get message from server.";
+      });
 
   return (
     <div className="App">
@@ -25,6 +37,8 @@ function App() {
         <p>This is a netlify test website.</p>
         <p>Message from server:</p>
         <p>{msg}</p>
+        <p>From serverless function:</p>
+        <p>{smsg}</p>
       </header>
     </div>
   );
